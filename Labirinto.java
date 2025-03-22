72e489d9864f66722e22509f2d4cee5cd8e2f0e6
Giovanni-Santomarco <gio.santomarco@stud.uniroma3.it>
2025-03-22 16:13:47 +0100

    Add files via upload

---
+++ Labirinto.java
@@ -0,0 +1,68 @@
+
+/**
+ * questa classe crea la mappa di gioco
+ */
+
+public class Labirinto {
+	
+	private Stanza stanzainiziale;
+	private Stanza stanzafinale;
+	
+	
+	public Labirinto() {
+		init();
+	}
+	
+	/**
+     * Crea tutte le stanze e le porte di collegamento
+     */
+	
+    public void init() {
+
+		/* crea gli attrezzi */
+    	Attrezzo lanterna = new Attrezzo("lanterna",3);
+		Attrezzo osso = new Attrezzo("osso",1);
+    	
+		/* crea stanze del labirinto */
+		Stanza atrio = new Stanza("Atrio");
+		Stanza aulaN11 = new Stanza("Aula N11");
+		Stanza aulaN10 = new Stanza("Aula N10");
+		Stanza laboratorio = new Stanza("Laboratorio Campus");
+		Stanza biblioteca = new Stanza("Biblioteca");
+		
+		/* collega le stanze */
+		atrio.impostaStanzaAdiacente("nord", biblioteca);
+		atrio.impostaStanzaAdiacente("est", aulaN11);
+		atrio.impostaStanzaAdiacente("sud", aulaN10);
+		atrio.impostaStanzaAdiacente("ovest", laboratorio);
+		aulaN11.impostaStanzaAdiacente("est", laboratorio);
+		aulaN11.impostaStanzaAdiacente("ovest", atrio);
+		aulaN10.impostaStanzaAdiacente("nord", atrio);
+		aulaN10.impostaStanzaAdiacente("est", aulaN11);
+		aulaN10.impostaStanzaAdiacente("ovest", laboratorio);
+		laboratorio.impostaStanzaAdiacente("est", atrio);
+		laboratorio.impostaStanzaAdiacente("ovest", aulaN11);
+		biblioteca.impostaStanzaAdiacente("sud", atrio);
+
+        /* pone gli attrezzi nelle stanze */
+		aulaN10.addAttrezzo(lanterna);
+		atrio.addAttrezzo(osso);
+
+		// il gioco comincia nell'atrio
+        stanzainiziale = atrio;  
+		stanzafinale = biblioteca;
+    }
+    
+    
+    public Stanza getStanzaIniziale() {
+    	return this.stanzainiziale;
+    }
+    
+    public Stanza getStanzaFinale() {
+    	return this.stanzafinale;
+    }
+    
+    
+    
+    
+}
